34d9a33e5a8c5b04eb3e62edd352a7a2b53f101a
Abhinav Anand <abhinav.anand18@gmail.com>
2024-10-28 08:23:14 +0000

    1st Commit

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,6 +7,7 @@ sap.ui.define(
 
     return BaseController.extend("zsuppregs.controller.App", {
       onInit: function () {
+        // for screen resolution.
         var zoom = ((window.outerWidth - 10) / window.innerWidth) * 100;
         if (zoom > 85) {
           document.body.style.zoom = "80%";
